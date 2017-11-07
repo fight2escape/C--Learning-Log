2682e82bfdcd7e8d5be362dffb8974294db62a8e
Escape <fight2escape@gmail.com>
2017-11-07 17:18:01 +0800

    检测线程状态

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,15 +15,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Program");
-            Thread t = new Thread(PrintNumbersWithDelay);
-            t.Start();
-            Thread.Sleep(TimeSpan.FromSeconds(6));
-            t.Abort();
-            Console.WriteLine("A thread has been aborted");
-            Thread t2 = new Thread(PrintNumbers);
+            Thread t1 = new Thread(PrintNumbersWithStatus);
+            Thread t2 = new Thread(DoNothing);
+            Console.WriteLine(t1.ThreadState.ToString());
             t2.Start();
-            PrintNumbers();
+            t1.Start();
+            for(int i=1; i<30; ++i)
+            {
+                Console.WriteLine(t1.ThreadState.ToString());
+            }
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+            t1.Abort();
+            Console.WriteLine("A thread has been aborted");
+            Console.WriteLine(t1.ThreadState.ToString());
+            Console.WriteLine(t2.ThreadState.ToString());
         }
+
+        static void DoNothing()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        static void PrintNumbersWithStatus()
+        {
+            Console.WriteLine("Starting With Status");
+            Console.WriteLine(Thread.CurrentThread.ThreadState.ToString());
+            for (int i = 0; i < 10; ++i)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
+        }
+
 
         static void PrintNumbersWithDelay()
         {
