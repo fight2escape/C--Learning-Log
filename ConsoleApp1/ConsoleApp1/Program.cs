26e154716accf3e68f60fa5e9eb118a1e4af7e12
Escape <fight2escape@gmail.com>
2017-11-10 10:55:04 +0800

    线程池

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,13 +15,32 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Current thread priority: {0}", Thread.CurrentThread.Priority);
-            Console.WriteLine("Running on all cores available");
-            RunThreads();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            Console.WriteLine("Running on a single core");
-            Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
-            RunThreads();
+            //Console.WriteLine("Current thread priority: {0}", Thread.CurrentThread.Priority);
+            //Console.WriteLine("Running on all cores available");
+            //RunThreads();
+            //Thread.Sleep(TimeSpan.FromSeconds(2));
+            //Console.WriteLine("Running on a single core");
+            //Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
+            //RunThreads();
+            Console.WriteLine("主线程ID = {0}", Thread.CurrentThread.ManagedThreadId);
+            ThreadPool.QueueUserWorkItem(CallBackWorkItem);
+            ThreadPool.QueueUserWorkItem(CallBackWorkItem, "work");
+            Thread.Sleep(3000);
+            Console.WriteLine("主线程退出");
+        }
+
+        private static void CallBackWorkItem(object state)
+        {
+            Console.WriteLine("线程池线程开始执行");
+            if (state != null)
+            {
+                Console.WriteLine("线程池线程ID = {0} 传入的参数为 {1}",
+                    Thread.CurrentThread.ManagedThreadId, state.ToString());
+            }
+            else
+            {
+                Console.WriteLine("线程池线程ID = {0}", Thread.CurrentThread.ManagedThreadId);
+            }
         }
 
        
