6d1a5ce57b93c87f7e79876206e4d507418a6232
Escape <fight2escape@gmail.com>
2017-11-10 11:05:19 +0800

    协作式取消线程

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -22,11 +22,38 @@ namespace ConsoleApp1
             //Console.WriteLine("Running on a single core");
             //Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
             //RunThreads();
-            Console.WriteLine("主线程ID = {0}", Thread.CurrentThread.ManagedThreadId);
-            ThreadPool.QueueUserWorkItem(CallBackWorkItem);
-            ThreadPool.QueueUserWorkItem(CallBackWorkItem, "work");
-            Thread.Sleep(3000);
-            Console.WriteLine("主线程退出");
+            Console.WriteLine("主线程运行");
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ThreadPool.QueueUserWorkItem(callback, cts.Token);
+            Console.WriteLine("按下回车来取消计数");
+            Console.Read();
+            cts.Cancel();
+            Console.ReadKey();
+        }
+
+        private static void callback(object state)
+        {
+            CancellationToken token = (CancellationToken)state;
+            Console.WriteLine("开始计数");
+            Count(token, 1000);
+        }
+
+        private static void Count(CancellationToken token, int countto)
+        {
+            for(int i=0; i<countto; ++i)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("计数取消");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("计数为：{0}", i);
+                    Thread.Sleep(300);
+                }
+            }
+            Console.WriteLine("计数完成");
         }
 
         private static void CallBackWorkItem(object state)
